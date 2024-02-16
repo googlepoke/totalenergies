23f0d4e0420c5d2a201639d057019293db9cbec1
Manie Cronje <manie.cronje@gmail.com>
2024-02-16 09:20:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var refererWarning = document.querySelector('.referer-warning');
+  if (refererWarning) refererWarning.style.display = 'none';
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Target the paragraph using its class name
+  var description = document.querySelector('.c-accordion__description');
+
+  // Add a click event listener to the paragraph
+  description.addEventListener('click', function() {
+    // Navigate to the URL in the same browser tab when the paragraph is clicked
+    window.location.href = 'https://ifs.super.site/northpower';
+  });
+});
